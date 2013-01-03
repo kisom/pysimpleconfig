1b2c76eb69bc7a13072fa09c0bd809769f38b086
Kyle Isom <coder@kyleisom.net>
2013-01-03 11:11:06 -0700

    add more metadata to setup script

--- setup.py
+++ setup.py
@@ -5,5 +5,10 @@ from distutils.core import setup
 setup(name='simpleconfig',
       version='1.0',
       py_modules=['simpleconfig'],
-      )
+      description=('Dead simple configuration file parser.'),
+      author='Kyle Isom',
+      author_email='coder@kyleisom.net',
+      license='ISC',
+      url='http://kisom.github.com/pysimpleconfig',
+)
 
